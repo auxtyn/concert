63475dadfa1321ce91c206a467e5accadce7d4f5
Auxtyn <austinserver042@gmail.com>
2025-08-16 20:06:03 +0100

    update

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 const HeroText = () => {
   // const words = ["Earth", "skies", "clouds", "birds", "plains", "cosmos"];
 
-  const words = ["Earth", "skies"];
+  const words = ["Earth"];
 
   const variants = {
     hidden: { opacity: 0, x: -50 },
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -11,54 +11,228 @@ const About = () => {
         <h1 className="headtext">
           1. Opening Hymn - I will enter his gates by Zitu Benafa
         </h1>
-        <p className="  text-blue-300">Conducted By Austin Ugwu</p>
+        <p className="  text-cyan-300">Conducted By Austin Ugwu</p>
         <p className="subtext mb-5">
-          Background: Written as a poem to accompany Beethoven’s “Ode to Joy,”
-          Van Dyke composed this hymn while inspired by the Berkshire Mountains.
-          He described it as a “hymn of trust and joy and hope,” meant to unite
-          people in worship regardless of scientific or social change.
+          Background: Adapted from Psalm 100:4, which encourages believers to
+          approach God with gratitude and praise. It signifies a posture of
+          worship and thankfulness as one approaches God's presence. This verse
+          is often used in Christian worship and devotional contexts as a
+          reminder to express appreciation for God's goodness and faithfulness.
         </p>
         <hr />
       </section>
 
       <section className="">
         <h1 className="headtext">2. Opening Prayer</h1>
-        <p className="  text-blue-300">By parish pastorial team</p>
+        <p className="  text-cyan-300">By parish pastorial team</p>
         <p className="subtext mb-5">The priest officially opens the event</p>
         <hr />
       </section>
       <section className="">
         <h1 className="headtext">3. Opening remark</h1>
-        <p className="  text-blue-300 mb-5">
+        <p className="  text-cyan-300 mb-5">
           By the committe chairman - Mbigbo Charles
         </p>
         <hr />
       </section>
       <section className="">
         <h1 className="headtext">4. Breaking of kolanut</h1>
-        {/* <p className="  text-blue-300 mb-5">
-          By the committe chairman - Mbigbo Charles
-        </p> */}
+        <p className="  text-cyan-300 mb-5">By the chairman - Chief Martin</p>
         <hr />
       </section>
 
       <section className="">
         <h2 className="text-heading mb-5 mt-5 pt-5 text-left">
-          Part 1 - <span className="text-blue-300 ">Hymns </span>
-          <br /><span className="text-2xl">conducted By Benedicta Obuh - CM</span>
+          Part 1 - <span className="text-cyan-300 ">Hymns </span>
+          <br />
+          <span className="text-2xl">conducted By Benedicta Obuh - CM</span>
         </h2>
 
-        <h1 className="headtext">
-          1. Opening Hymn - I will enter his gates by Zitu Benafa
-        </h1>
-        <p className="  text-blue-300">Conducted By Austin Ugwu</p>
+        <h1 className="headtext">● Joyful, joyful we adore Thee</h1>
+        <p className="  text-cyan-300">Composed by Henry Van Dyke (1907)</p>
         <p className="subtext mb-5">
-          Background: Written as a poem to accompany Beethoven’s “Ode to Joy,”
-          Van Dyke composed this hymn while inspired by the Berkshire Mountains.
-          He described it as a “hymn of trust and joy and hope,” meant to unite
-          people in worship regardless of scientific or social change.
+          <span className="text-cyan-300">Background: </span> Written as a poem
+          to accompany Beethoven’s “Ode to Joy,” Van Dyke composed this hymn
+          while inspired by the Berkshire Mountains. He described it as a “hymn
+          of trust and joy and hope,” meant to unite people in worship
+          regardless of scientific or social change.
+        </p>
+
+        <h1 className="headtext">● O God our help in ages past</h1>
+        <p className="  text-cyan-300">Composed by Isaac Watts (1714)</p>
+        <p className="subtext mb-5">
+          <span className="text-cyan-300">Background: </span> A paraphrase of
+          Psalm 90, this hymn was written during a time of political and
+          religious turmoil in England. Watts offered comfort and assurance in
+          God’s eternal presence, contrasting human frailty with divine
+          permanence.
+        </p>
+        <h1 className="headtext">● For the beauty of The Earth</h1>
+        <p className="  text-cyan-300">Composed by John Rutter (1714)</p>
+        <p className="subtext mb-5">
+          <span className="text-cyan-300">Background: </span> A paraphrase of
+          Psalm 90, this hymn was written during a time of political and
+          religious turmoil in England. Watts offered comfort and assurance in
+          God’s eternal presence, contrasting human frailty with divine
+          permanence.
+        </p>
+        <h1 className="headtext">● O God beyond all praising</h1>
+        <p className="  text-cyan-300">Composed Michael Perry (1982)</p>
+        <p className="subtext mb-5">
+          <span className="text-cyan-300">Background: </span> This hymn was
+          written to express awe and reverence for God’s gifts. Perry’s lyrics
+          reflect biblical themes and personal resilience, making it a powerful
+          anthem of praise.
+        </p>
+
+        <span className="text-2xl">
+          Songs for our deceased relatives and loved ones
+        </span>
+        <h1 className="headtext">● Finally home</h1>
+        <p className="  text-cyan-300">Composed Michael Perry (1982)</p>
+        <p className="subtext mb-5">
+          <span className="text-cyan-300">Background: </span> This hymn was
+          written to express awe and reverence for God’s gifts. Perry’s lyrics
+          reflect biblical themes and personal resilience, making it a powerful
+          anthem of praise.
+        </p>
+        <h1 className="headtext">● It Is Well with My Soul</h1>
+        <p className="  text-cyan-300">
+          Composed by Composer: Horatio Spafford (1873)
+        </p>
+        <p className="subtext mb-5">
+          <span className="text-cyan-300">Background: </span> Penned after
+          Spafford lost his four daughters in a shipwreck, this hymn is a
+          profound declaration of faith amid tragedy. Its enduring message of
+          peace and trust in God has comforted generations.
+        </p>
+
+        <hr />
+      </section>
+
+      <section className="">
+        <h2 className="text-heading mb-5 mt-5 pt-5 text-left">
+          Novelty singing
+          <br />
+          {/* <span className="text-2xl">conducted By Benedicta Obuh - CM</span> */}
+        </h2>
+
+        <h1 className="headtext">● Songs from the audience</h1>
+        {/* <p className="  text-cyan-300">Composed by Henry Van Dyke (1907)</p> */}
+        <p className="subtext mb-5">
+          <span className="text-cyan-300"></span> The audience sings together,
+          producing beautiful sounds of worship to God
+        </p>
+
+        <h1 className="headtext">Children's choir performance</h1>
+        <h1 className="headtext">● Circle of life</h1>
+
+        <p className="  text-cyan-300">
+          Composed by Elton John & Hans Zimmer (1994)
+        </p>
+        <p className="subtext mb-5">
+          <span className="text-cyan-300">Background: </span> The Lion King,
+          “Circle of Life” is a powerful anthem that explores themes of birth,
+          death, and renewal. The song begins with a striking Zulu chant,
+          setting the tone for a story rooted in nature and destiny. Elton John
+          composed the melody in a burst of inspiration, reportedly completing
+          it within hours of receiving Tim Rice’s lyrics. With its sweeping
+          orchestration and universal message, “Circle of Life” has become a
+          cultural touchstone, symbolizing the interconnectedness of all living
+          things.
+        </p>
+        <h1 className="headtext">Solo performances</h1>
+        {/* <p className="  text-cyan-300">Composed by John Rutter (1714)</p> */}
+        <p className="subtext mb-5">
+          {/* <span className="text-cyan-300">Background: </span>  */}
+          Soloists render beautiful songs
+        </p>
+
+        <hr />
+
+        <h1 className="headtext">Induction of patrons / Awards</h1>
+        {/* <p className="  text-cyan-300">Composed by Henry Van Dyke (1907)</p> */}
+        <p className="subtext mb-5">
+          <span className="text-cyan-300">By the parish priest</span>
         </p>
         <hr />
+      </section>
+
+      <section className="">
+        <h2 className="text-heading mb-5 mt-5 pt-5 text-left">
+          Part 2 - <span className="text-cyan-300 ">Contemporary songs </span>
+          <br />
+          <span className="text-2xl">conducted By Benedicta Obuh - CM</span>
+        </h2>
+
+        <h1 className="headtext">● Control</h1>
+        {/* <p className="  text-cyan-300">Composed by Henry Van Dyke (1907)</p> */}
+        <p className="subtext mb-5">
+          <span className="text-cyan-300">Background: </span> A soulful
+          declaration of surrender, “Control” reflects the believer’s desire to
+          let go and allow God to take the lead. With its smooth rhythm and
+          heartfelt lyrics, the song resonates with anyone navigating
+          uncertainty, reminding them that divine guidance brings peace and
+          purpose.
+        </p>
+
+        <h1 className="headtext">● Hail Baba God</h1>
+        <p className="  text-cyan-300">Composed by Onwubuya Charles</p>
+        <p className="subtext mb-5">
+          <span className="text-cyan-300">Background: </span> This vibrant
+          praise anthem celebrates God’s goodness with joyful Nigerian
+          expressions. Drawing from everyday gratitude, the song uses phrases
+          like “I throway salute” to honor God in a culturally rich and
+          musically uplifting way. It’s a favorite in worship gatherings for its
+          infectious energy and heartfelt devotion.
+        </p>
+
+        <hr />
+        <section className="">
+          <h2 className="text-heading mb-5 mt-5 pt-5 text-left">
+            Part 3 - <span className="text-cyan-300 ">Anthems </span>
+            <br />
+            <span className="text-2xl">conducted By Benedicta Obuh - CM</span>
+          </h2>
+
+          <h1 className="headtext">● Lift up your heads, O ye gates</h1>
+          <p className="  text-cyan-300">Composed by Georg Weissel (1642)</p>
+          <p className="subtext mb-5">
+            <span className="text-cyan-300">Background: </span> Inspired by Psalm 24, Weissel wrote this hymn during the Thirty Years’ War, calling believers to open their hearts to the “King of Glory.” The anthem’s majestic tone and scriptural depth make it a powerful call to worship and spiritual readiness.
+          </p>
+
+          <h1 className="headtext">● How Excellent Is Thy Loving Kindness</h1>
+          <p className="  text-cyan-300">Composed by Charles F. Noyes (Date uncertain, likely 19th century)</p>
+          <p className="subtext mb-5">
+            <span className="text-cyan-300">Background: </span> Based on Psalm 36:7, this hymn praises God’s mercy and protection. Its lyrical beauty and rich harmonies evoke a sense of divine shelter and satisfaction, making it a cherished piece in both African and Western sacred music traditions
+            permanence.
+          </p>
+          <h1 className="headtext">● Come let us sing</h1>
+          <p className="  text-cyan-300">Composed by James Varrick Armah</p>
+          <p className="subtext mb-5">
+            <span className="text-cyan-300">Background: </span> Originally written for children, this hymn invites all to rise in praise. Wesley’s poetic style blends biblical themes with accessible language, making it suitable for worshippers of all ages. It’s a joyful celebration of creation, resurrection, and divine love.
+          </p>
+          <h1 className="headtext">● Hail Smiling Morn</h1>
+          <p className="  text-cyan-300">Composed by Reginald Spofforth (1810)</p>
+          <p className="subtext mb-5">
+            <span className="text-cyan-300">Background: </span> A classic English glee, this piece is often sung as a carol during Christmas or Easter. Its cheerful melody and poetic imagery of dawn breaking symbolize hope and renewal. Popular in Yorkshire pub traditions, it remains one of the most beloved glees in choral history.
+            powerful anthem of praise.
+          </p>
+          <h1 className="headtext">● Day by Day, We Magnify Thee</h1>
+          <p className="  text-cyan-300">John Ellerton (1858)</p>
+          <p className="subtext mb-5">
+            <span className="text-cyan-300">Background: </span> Written for daily school worship, this hymn emphasizes consistent praise through work, prayer, and obedience. Ellerton’s gentle verses encourage a life of devotion, culminating in the hope of eternal worship.
+          </p>
+          <h1 className="headtext">● And We Worship Thy Name</h1>
+          <p className="  text-cyan-300">
+            Composer: Unknown
+          </p>
+          <p className="subtext mb-5">
+            <span className="text-cyan-300">Background: </span> Though specific origins are unclear, this anthem reflects themes of reverence and adoration. Often performed in modern gospel settings, it emphasizes collective worship and the majesty of God’s name, drawing from Psalms and Revelation for its lyrical inspiration.
+          </p>
+
+          <hr />
+        </section>
       </section>
     </section>
   );
